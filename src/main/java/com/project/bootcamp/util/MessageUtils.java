4661e92dbc792a2d922ec07100f0455617b54d0b
João Pedro Hegedus Vellenich <jpvellenich@gmail.com>
2021-05-27 18:58:19 -0300

    Backend

---
+++ src/main/java/com/project/bootcamp/util/MessageUtils.java
@@ -0,0 +1,6 @@
+package com.project.bootcamp.util;
+
+public abstract class MessageUtils {
+    public static final String STOCK_ALREADY_EXISTS = "Stock alredy exists in the database";
+    public static final String STOCK_NOT_FOUND = "Stock dosen't exist";
+}
